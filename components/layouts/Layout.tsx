8785a0bbd63667d3898abcb8a469dad1abb39759
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-01-22 02:06:34 +0800

    update folder structure

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 import styles from './Layout.module.scss';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import Header from './Header';
+import Footer from './Footer';
 
 const Layout: NextPage = ({ children }) => {
 	return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Layout from '../components/layouts/Layout/Layout';
+import Layout from '../components/layouts/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
