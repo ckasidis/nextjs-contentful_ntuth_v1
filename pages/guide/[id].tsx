7a10e75c4acf42d6d9c4b7cf327561eb1beb373e
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-01-24 22:36:42 +0800

    create contentful client in guide/[id]

--- pages/guide/[id].tsx
+++ pages/guide/[id].tsx
@@ -1,7 +1,27 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import { createClient } from 'contentful';
 
-const SingleGuidePage: NextPage = () => {
-	return <div>Single Guide Page</div>;
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+	const client = createClient({
+		space: process.env.CONTENTFUL_SPACE_ID!,
+		environment: process.env.CONTENTFUL_ENVIRONMENT_ID!,
+		accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+	});
+
+	const res = await client.getEntries({
+		content_type: 'guide',
+		'fields.slug': params!.id,
+	});
+
+	return {
+		props: {
+			guide: res.items[0],
+		},
+	};
+};
+
+const SingleGuidePage: NextPage = ({ guide }: any) => {
+	return <div>Single Guide Page{guide.fields.title}</div>;
 };
 
 export default SingleGuidePage;
