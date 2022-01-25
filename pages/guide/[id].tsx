d6980c50bb710b6d5e96c68b0fc7f0dab4e615de
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-01-26 01:21:10 +0800

    outputting data from contentful in guide/[id]

--- pages/guide/[id].tsx
+++ pages/guide/[id].tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps, NextPage } from 'next';
+import Image from 'next/image';
 import contentful from '../../lib/contentful';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { IGuide } from '../../@types/generated/contentful';
 import PageNotFound from '../404';
 
@@ -22,7 +24,40 @@ interface Props {
 
 const SingleGuidePage: NextPage<Props> = ({ guide }) => {
 	if (!guide) return <PageNotFound />;
-	return <div>Single Guide Page{guide.fields.title}</div>;
+
+	const { title, featuredImage, mainContent, author } = guide.fields;
+	console.log(guide);
+
+	return (
+		<main className="main">
+			<h1 className="title">{title}</h1>
+			<div className="author">
+				{author.map((member) => (
+					<h3 key={member.fields.fullname}>{member.fields.fullname}</h3>
+				))}
+			</div>
+			<div className="featured-image">
+				<Image
+					src={'http:' + featuredImage.fields.file.url}
+					width={featuredImage.fields.file.details.image!.width}
+					height={featuredImage.fields.file.details.image!.height}
+					alt={featuredImage.fields.title}
+				/>
+			</div>
+			<div className="main-content">
+				{documentToReactComponents(mainContent!)}
+			</div>
+			<style jsx>{`
+				.main {
+					min-height: inherit;
+				}
+
+				.featured-image {
+					width: clamp(20rem, 60vw, 40rem);
+				}
+			`}</style>
+		</main>
+	);
 };
 
 export default SingleGuidePage;
