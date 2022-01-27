b64bd3ae6bb99b694bdac452192d22364b3e2a54
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-01-28 00:11:42 +0800

    add list of guides to guide page

--- pages/guide/index.tsx
+++ pages/guide/index.tsx
@@ -1,7 +1,32 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import Link from 'next/link';
+import { IGuide } from '../../@types/generated/contentful';
+import contentful from '../../lib/contentful';
 
-const index: NextPage = () => {
-	return <div>Guide Page</div>;
+export const getServerSideProps: GetServerSideProps = async () => {
+	const res = await contentful.getEntries({
+		content_type: 'guide',
+	});
+	return {
+		props: { guides: res.items },
+	};
+};
+
+interface Props {
+	guides: IGuide[];
+}
+
+const index: NextPage<Props> = ({ guides }) => {
+	return (
+		<main>
+			<h1>Guide Page</h1>
+			{guides.map((guide) => (
+				<h2 key={guide.fields.title}>
+					<Link href={`/guide/${guide.fields.slug}`}>{guide.fields.title}</Link>
+				</h2>
+			))}
+		</main>
+	);
 };
 
 export default index;
