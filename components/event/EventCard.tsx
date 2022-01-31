d2675a0929152a64706a23939146e14c2c1e4263
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-02-01 00:00:41 +0800

    fix style jsx conflict with motion components

--- components/event/EventCard.tsx
+++ components/event/EventCard.tsx
@@ -12,40 +12,37 @@ const EventCard: NextPage<Props> = ({ event }) => {
 	const { title, slug, shortDescription, featuredImage } = event.fields;
 
 	return (
-		<Link href={`/event/${slug}`} passHref={true}>
-			<motion.div
-				className="card container"
-				whileHover={{ scale: 1.05 }}
-				whileTap={{ scale: 0.95 }}
-			>
-				<div className="image">
-					<Image
-						className="rounded"
-						src={'http:' + featuredImage.fields.file.url}
-						width={featuredImage.fields.file.details.image!.width}
-						height={featuredImage.fields.file.details.image!.height}
-						alt={featuredImage.fields.title}
-					/>
-				</div>
-				<div className="container">
-					<h4>{title}</h4>
-					<p>{shortDescription}</p>
-				</div>
-				<style jsx>{`
-					.container {
-						display: grid;
-						grid-template-columns: 1fr;
-						gap: 1rem;
-						text-align: center;
-						max-width: 25em;
-					}
-					.image {
-						width: 80%;
-						margin-inline: auto;
-					}
-				`}</style>
-			</motion.div>
-		</Link>
+		<>
+			<Link href={`/event/${slug}`} passHref={true}>
+				<motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+					<div className="card container">
+						<div className="image">
+							<Image
+								className="rounded"
+								src={'http:' + featuredImage.fields.file.url}
+								width={featuredImage.fields.file.details.image!.width}
+								height={featuredImage.fields.file.details.image!.height}
+								alt={featuredImage.fields.title}
+							/>
+						</div>
+						<h4>{title}</h4>
+						<p>{shortDescription}</p>
+					</div>
+				</motion.div>
+			</Link>
+			<style jsx>{`
+				.container {
+					display: grid;
+					grid-template-columns: 1fr;
+					place-items: center;
+					gap: 1rem;
+					max-width: 25em;
+				}
+				.image {
+					width: 80%;
+				}
+			`}</style>
+		</>
 	);
 };
 
--- components/guide/GuideCard.tsx
+++ components/guide/GuideCard.tsx
@@ -12,40 +12,40 @@ const GuideCard: NextPage<Props> = ({ guide }) => {
 	const { title, slug, shortDescription, featuredImage } = guide.fields;
 
 	return (
-		<Link href={`/guide/${slug}`} passHref={true}>
-			<motion.div
-				className="card container"
-				whileHover={{ scale: 1.05 }}
-				whileTap={{ scale: 0.95 }}
-			>
-				<div className="image">
-					<Image
-						className="rounded"
-						src={'http:' + featuredImage.fields.file.url}
-						width={featuredImage.fields.file.details.image!.width}
-						height={featuredImage.fields.file.details.image!.height}
-						alt={featuredImage.fields.title}
-					/>
-				</div>
-				<div className="container">
-					<h4>{title}</h4>
-					<p>{shortDescription}</p>
-				</div>
-				<style jsx>{`
-					.container {
-						display: grid;
-						grid-template-columns: 1fr;
-						gap: 1rem;
-						text-align: center;
-						max-width: 25em;
-					}
-					.image {
-						width: 80%;
-						margin-inline: auto;
-					}
-				`}</style>
-			</motion.div>
-		</Link>
+		<>
+			<Link href={`/guide/${slug}`} passHref={true}>
+				<motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+					<div className="card container">
+						<div className="image">
+							<Image
+								className="rounded"
+								src={'http:' + featuredImage.fields.file.url}
+								width={featuredImage.fields.file.details.image!.width}
+								height={featuredImage.fields.file.details.image!.height}
+								alt={featuredImage.fields.title}
+							/>
+						</div>
+						<div className="container">
+							<h4>{title}</h4>
+							<p>{shortDescription}</p>
+						</div>
+					</div>
+				</motion.div>
+			</Link>
+			<style jsx>{`
+				.container {
+					display: grid;
+					grid-template-columns: 1fr;
+					gap: 1rem;
+					text-align: center;
+					max-width: 25em;
+				}
+				.image {
+					width: 80%;
+					margin-inline: auto;
+				}
+			`}</style>
+		</>
 	);
 };
 
