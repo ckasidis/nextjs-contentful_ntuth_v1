f83fc3162cd7faec6a812fda7a53e5b07357fcb6
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-02-01 13:37:14 +0800

    add page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { motion } from 'framer-motion';
 import Layout from '../components/layouts/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+	const pageTransition = {
+		hidden: {
+			opacity: 0,
+		},
+		visible: {
+			opacity: 1,
+		},
+	};
+
 	return (
 		<Layout>
-			<Component {...pageProps} />
+			<motion.div
+				key={router.route}
+				variants={pageTransition}
+				initial="hidden"
+				animate="visible"
+			>
+				<Component {...pageProps} />
+			</motion.div>
 		</Layout>
 	);
 }
