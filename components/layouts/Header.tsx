3b445c38aecd16bc37fd11135a2d84b3fbf8e0f1
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-02-04 22:28:41 +0800

    modify Mobile Navigation in Header component

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -2,7 +2,6 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import MobileNavigation from './MobileNavigation';
 
 const Header: NextPage = () => {
 	const [open, setOpen] = useState(false);
@@ -11,31 +10,6 @@ const Header: NextPage = () => {
 		setOpen(!open);
 	};
 
-	const menuItems = (
-		<>
-			<li>
-				<Link href="/guide">
-					<a className="nav-link">guides</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/event">
-					<a className="nav-link">events</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/member">
-					<a className="nav-link">members</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/about">
-					<a className="nav-link">about</a>
-				</Link>
-			</li>
-		</>
-	);
-
 	return (
 		<header className="header">
 			<div className="logo">
@@ -43,20 +17,37 @@ const Header: NextPage = () => {
 					<a className="logo">logo</a>
 				</Link>
 			</div>
-
-			{open && (
-				<MobileNavigation close={() => setOpen(!open)}>
-					{menuItems}
-				</MobileNavigation>
-			)}
-
-			<nav>
-				<button className="hamburger-menu" onClick={toggleMenu}>
-					<span className="sr-only">Menu</span>
-					<GiHamburgerMenu />
-				</button>
-				<ul className="list">{menuItems}</ul>
-			</nav>
+			<div className={`${open ? 'backdrop' : ''}`} onClick={toggleMenu}>
+				<nav className={`${open ? 'nav-expand' : ''}`}>
+					<ul className={`list ${open ? 'list-expand' : ''}`}>
+						{' '}
+						<li>
+							<Link href="/guide">
+								<a className="nav-link">guides</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="/event">
+								<a className="nav-link">events</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="/member">
+								<a className="nav-link">members</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="/about">
+								<a className="nav-link">about</a>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<button className="hamburger-menu" onClick={toggleMenu}>
+				<span className="sr-only">Menu</span>
+				<GiHamburgerMenu />
+			</button>
 			<style jsx>{`
 				.header {
 					display: flex;
@@ -74,6 +65,10 @@ const Header: NextPage = () => {
 					color: var(--link-alternate);
 				}
 
+				.nav-link {
+					color: var(--link-alternate);
+				}
+
 				.list {
 					display: flex;
 					gap: 1rem;
@@ -81,15 +76,42 @@ const Header: NextPage = () => {
 
 				.hamburger-menu {
 					display: none;
-					border: none;
 				}
 
 				@media (max-width: 40em) {
+					.hamburger-menu {
+						display: block;
+						border: none;
+					}
+
 					.list {
 						display: none;
 					}
-					.hamburger-menu {
-						display: block;
+
+					.nav-expand {
+						position: fixed;
+						top: 0;
+						right: 0;
+						background-color: var(--primary);
+						height: 100vh;
+						width: min(60vw, 20rem);
+					}
+
+					.list-expand {
+						padding-top: 4rem;
+						display: flex;
+						flex-direction: column;
+						gap: 1rem;
+						align-items: center;
+					}
+
+					.backdrop {
+						position: fixed;
+						top: 0;
+						left: 0;
+						width: 100vw;
+						height: 100vh;
+						background-color: rgba(0, 0, 0, 0.75);
 					}
 				}
 			`}</style>
