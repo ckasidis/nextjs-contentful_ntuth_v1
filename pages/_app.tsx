b1dfd8e0e263ffe7c62c1ba3e59e884c16f3a549
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-02-11 11:16:36 +0800

    add icon to title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
 			<Head>
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="icon" href="/NTUTH.ico" />
 			</Head>
 			<Header />
 			<motion.div
